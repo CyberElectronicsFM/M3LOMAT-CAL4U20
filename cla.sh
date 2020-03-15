9302ea2d6f8bfd40d10824ee200b8146f5906ee6
hitlerhack313 <59124860+hitlerhack313@users.noreply.github.com>
2020-03-15 17:54:59 +0200

    Add files via upload

---
+++ cla.sh
@@ -0,0 +1,124 @@
+#                               colors
+clear
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+#echo -n "$red"
+#                          start programing
+    echo -e "$green"
+    echo "  ╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╗"
+    echo "  ╢                ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄                 ╟"
+    echo "  ╢                ████▌▄▌▄▐▐▌█████                 ╟"
+    echo "  ╢                ████▌▄▌▄▐▐▌▀████                 ╟"
+    echo "  ╢                ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀                 ╟"
+    echo "  ╢               ━━━━▣━━◤◢━━▣━━━━━                 ╟"
+    echo "  ╢           【ツ】   WELCOME  【ツ】              ╟"
+    echo "  ╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╗"
+    echo "  ╢                                                 ╟"
+    echo "  ╢               Coded By: MENA MAGDY              ╟"
+    echo "  ╢                                                 ╟"
+    echo "  ╢____This tool is designed via BASH  language ____╟"
+    echo "  ╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╗"
+    echo "  ╢________ Designed for Termux & Kali users________╟"
+    echo "  ╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╗"
+    echo "  ╢__For more tools and explanations follow us on___╟"
+    echo "   _________________________________________________ "
+    echo "  ╢                                                 ╟"
+    echo "  ╢                YouTube channel:                 ╟"
+    echo "  ╢                                                 ╟"
+    echo "  ╢_____________https://youtu.be/iTaZJtXLeaE________╟"
+    echo "  ___________________________________________________"
+    echo " ╢                                                   ╟"
+    echo " ╢                    TELEGRAM:                      ╟"
+    echo " ╢                   JOIN US NOW                     ╟"
+    echo " ╢   https://t.me/joinchat/NYmlVhPbCU9tfGxAeSaZ3A    ╟"
+    echo " ╢                                                   ╟"
+    echo " ╚╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╤╧╤╝"
+    echo ""
+    echo " +++++++++++++++++++++++++++++++++++++++++++++++++++++"
+#   echo -n "$red"
+    echo "                 CODED BY MENA MAGDY                  "
+#   echo -x "$green"
+    echo " +++++++++++++++++++++++++++++++++++++++++++++++++++++"
+#pkg install git
+#pkg install python
+echo "  "
+#echo "   B             ┈┈┈╱▔▔▔▔▔▔╲┈╭━━━╮┈┈       y  "
+#echo "	   Y   	        ┈┈▕┈╭━╮╭━╮┈▏┃1+1=?.┃┈┈   y   "
+#echo "	     M	        ┈┈▕╭╯ ╮ ┃ ┈▏╰┳━━╯┈┈     d    "
+#echo "		e       ┈┈▕┈╰╯╭╮╰╯┈▏┈┃┈┈┈┈┈    g     "
+#echo "	          n     ┈┈▕┈┈┈┃┃┈┈┈▏━╯┈┈┈┈┈   a      "
+#echo "		    a   ┈┈▕┈┈┈╰╯┈┈┈▏┈┈┈┈┈┈┈  m       "
+#echo "		        ┈┈▕╱╲╱╲╱╲╱╲▏┈┈┈┈┈┈┈          "
+
+echo ""
+echo ""
+echo "_____________________    "
+echo "╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╧╤╧╗   "
+echo "╢ENTER NUMBER ONE :  ╟   "
+echo "╚╤╧╤╧╤╧╤╤╧╤╧╤╧╤╧╤╤╧╧╤╝   "
+echo ""
+echo "&&&&&&&&&&&&&&&&&&&&&"
+read numone
+echo "&&&&&&&&&&&&&&&&&&&&&"
+
+
+
+
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "╔╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╤╧╧╤╧╧╗"
+echo "╢ENTER ANTHER NUMBER :╟"
+echo "╚╤╧╤╧╤╧╤╤╧╤╧╤╧╤╧╤╤╧╧╧╤╝"
+echo ""
+echo "&&&&&&&&&&&&&&&&&&&&&"
+read numtwo
+echo "&&&&&&&&&&&&&&&&&&&&&"
+
+
+echo ""
+echo ""
+echo ""
+echo ""
+#                           start expration 
+
+      	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆"
+	echo "◇ WHEN { + } & PLUS = " `expr $numone + $numtwo`
+	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆"
+	echo ""
+
+	echo ""
+	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆◇"
+	echo "◇ WHEN { - } & MINUS = " `expr $numone - $numtwo`
+	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆◇"
+	echo ""
+
+
+	echo ""
+	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆◇"
+	echo "◇ WHEN { / } & DIVISTION = " `expr $numone \* $numtwo`
+	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆◇"
+	echo ""
+
+	echo ""
+	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆◇"
+	echo "◇ WHEN { * } & BY =       " `expr $numone / $numtwo`
+	echo "◇◆◇◆◇◆◇◆◇◆◇◇◆◇◆◇◆◇◆◇◆◇◆◇"
+	echo ""
+#time 30
+#echo "wel"
+#bash cla.sh
+exit 
+
+#echo "god pyyyy"
